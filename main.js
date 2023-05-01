0d01a3d44da8405ee4c38f7f4cc232cceb9acd60
Milan <milan@Milans-MacBook-Air.local>
2023-05-01 17:45:55 +0530

    added couponcode

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 let mode = document.getElementById('light')
 let banner = document.querySelector('.banner')
 let body = document.querySelector('.amazon')
-let categories = document.getElementsByClassName('categories');
+let categories = document.getElementsByClassName('categories')
+let coupon = document.querySelector('#coupon')
 
-const lightDark = ()=>{
+window.onload(coupon.classList.add('coupon'))
+
+function hideCoupon(){
+    coupon.classList.remove('coupon');
+    body.style.filter='blur(0px)';
+    console.log('a')
+}
+
+function lightDark() {
     banner.classList.toggle('dark')
     body.classList.toggle('dark')
     categories[0].classList.toggle('dark')
